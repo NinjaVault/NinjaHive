73e0db2866ac4a56d3151b1edbca193d0ada4ca0
Ruggero Visintin <ruggerovisintin@gmail.com>
2015-07-25 18:52:40 +0200

    Updated GameItemDto

--- NinjaHive.Contract/DTOs/GameItem.cs
+++ NinjaHive.Contract/DTOs/GameItem.cs
@@ -4,7 +4,17 @@ namespace NinjaHive.Contract.DTOs
 {
     public class GameItem
     {
-        public Guid ItemId { get; set; }
-        public string Name { get; set; }
+        public Guid ItemId                  { get; set; }
+
+        public string Name                  { get; set; }
+        public string Description           { get; set; }
+        public string Category              { get; set; }
+
+        public bool Craftable               { get; set; }
+        public bool UpgradeElement          { get; set; }
+        public bool CraftingElement         { get; set; }
+        public bool QuestItem               { get; set; }
+
+        public int Value                    { get; set; }
     }
 }
