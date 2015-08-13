2c8bee4c1577449349c45df4012f705bd50ed686
Ruggero Visintin <ruggerovisintin@gmail.com>
2015-08-13 23:20:25 +0200

    Added "AddEquipmentItemCommand"

---
+++ NinjaHive.BusinessLayer/CommandHandlers/AddEquipmentItemCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NinjaHive.Contract.Commands;
+using NinjaHive.Core;
+using NinjaHive.Domain;
+
+namespace NinjaHive.BusinessLayer.CommandHandlers
+{
+    class AddEquipmentItemCommandHandler : ICommandHandler<AddEquipmentItemCommand>
+    {
+        private readonly NinjaHiveContext db;
+
+        public AddEquipmentItemCommandHandler(NinjaHiveContext db)
+        {
+            this.db = db;
+        }
+
+        public void Handle(AddEquipmentItemCommand command)
+        {
+            var equipItem = new EquipmentItemEntity
+            {
+                Id = command.EquipmentItem.Id,
+                Name = command.EquipmentItem.Name,
+                Category = command.EquipmentItem.Category,
+                Description = command.EquipmentItem.Description,
+                Craftable = command.EquipmentItem.Craftable,
+                IsUpgrader = command.EquipmentItem.UpgradeElement,
+                IsCrafter = command.EquipmentItem.CraftingElement,
+                IsQuestItem = command.EquipmentItem.QuestItem,
+                Value = command.EquipmentItem.Value,
+                Durability = command.EquipmentItem.Durability,
+            };
+
+            this.db.GameItemEntities.Add(equipItem);
+        }
+    }
+}
