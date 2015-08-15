a4dfcc56d122e7e737ea1b421831f4ac8aa22d17
Alper Aslan <Qu4ntumHive@gmail.com>
2015-08-15 13:37:02 +0200

    refactoring of GetAllEquipmentItemsQueryHandler 1. for readability 2. for performance

--- NinjaHive.BusinessLayer/QueryHandlers/GetAllEquipmentItemsQueryHandler.cs
+++ NinjaHive.BusinessLayer/QueryHandlers/GetAllEquipmentItemsQueryHandler.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections.Generic;
 using System.Linq;
 using NinjaHive.Contract.DTOs;
 using NinjaHive.Contract.Queries;
@@ -20,36 +19,32 @@ namespace NinjaHive.BusinessLayer.QueryHandlers
 
         public EquipmentItem[] Handle(GetAllEquipmentItemsQuery query)
         {
-            var equipmentItems = this.db.GameItemEntities.ToArray();
-            ArrayList result = new ArrayList();
+            var equipmentItems = this.GetEquipmentItems();
 
+            return this.MapEquipmentItems(equipmentItems).ToArray();
+        }
 
+        private IQueryable<EquipmentItemEntity> GetEquipmentItems()
+        {
+            var equipmentItems = this.db.GameItemEntities.OfType<EquipmentItemEntity>();
+            return equipmentItems;
+        }
 
-            for (int i = 0; i < equipmentItems.ToArray().Length; i++)
+        private IEnumerable<EquipmentItem> MapEquipmentItems(IQueryable<EquipmentItemEntity> equipmentItems)
+        {
+            return equipmentItems.Select(equipmentItem => new EquipmentItem
             {
-                if (equipmentItems[i].GetType().BaseType == typeof(EquipmentItemEntity))                
-                {
-                    var equipmentItem = (EquipmentItemEntity)equipmentItems.ElementAt(i);
-                   
-                    result.Add(new EquipmentItem
-                    {
-                       Id = equipmentItem.Id,
-                       Name = equipmentItem.Name,
-                       Category = equipmentItem.Category,
-                       Description = equipmentItem.Description,
-                       Craftable = equipmentItem.Craftable,
-                       UpgradeElement = equipmentItem.IsUpgrader,
-                       CraftingElement = equipmentItem.IsCrafter,
-                       QuestItem = equipmentItem.IsQuestItem,
-                       Value = equipmentItem.Value,
-                       Durability = equipmentItem.Durability,
-                    });
-                }
-            }
-
-            EquipmentItem[] array = result.Cast<EquipmentItem>().ToArray<EquipmentItem>();
-
-            return array;
+                Id = equipmentItem.Id,
+                Name = equipmentItem.Name,
+                Category = equipmentItem.Category,
+                Description = equipmentItem.Description,
+                Craftable = equipmentItem.Craftable,
+                UpgradeElement = equipmentItem.IsUpgrader,
+                CraftingElement = equipmentItem.IsCrafter,
+                QuestItem = equipmentItem.IsQuestItem,
+                Value = equipmentItem.Value,
+                Durability = equipmentItem.Durability,
+            });
         }
     }
 }
