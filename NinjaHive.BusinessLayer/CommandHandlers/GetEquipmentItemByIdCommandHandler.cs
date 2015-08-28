12880e82e6ced054ba7c0b21c5229f36514f9948
Ruggero Visintin <ruggerovisintin@gmail.com>
2015-08-28 20:10:17 +0200

    Added GetEquipmenItemtByIdCommand and GetEquipmentItemByIdCommandHandler

---
+++ NinjaHive.BusinessLayer/CommandHandlers/GetEquipmentItemByIdCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NinjaHive.Contract.DTOs;
+using NinjaHive.Contract.Commands;
+using NinjaHive.Core;
+using NinjaHive.Domain;
+
+
+namespace NinjaHive.BusinessLayer.CommandHandlers
+{
+    public class GetEquipmentItemByIdCommandHandler
+    {
+        private readonly NinjaHiveContext db;
+
+        public GetEquipmentItemByIdCommandHandler(NinjaHiveContext db)
+        {
+            this.db = db;
+        }
+
+        public EquipmentItem Handle(GetEquipmentItemByIdCommand command)
+        {
+            var equipmentItem = (EquipmentItemEntity)db.GameItemEntities.Find(command.EquipmentId);
+
+            return new EquipmentItem
+            {
+                Id = equipmentItem.Id,
+                Name = equipmentItem.Name,
+                Category = equipmentItem.Category,
+                Description = equipmentItem.Description,
+                Craftable = equipmentItem.Craftable,
+                UpgradeElement = equipmentItem.IsUpgrader,
+                CraftingElement = equipmentItem.IsCrafter,
+                QuestItem = equipmentItem.IsQuestItem,
+                Value = equipmentItem.Value,
+                Durability = equipmentItem.Durability,
+            };
+        }
+
+        private IQueryable<EquipmentItemEntity> GetEquipmentItems()
+        {
+            var equipmentItems = this.db.GameItemEntities.OfType<EquipmentItemEntity>();
+            return equipmentItems;
+        }
+    }
+}
---
+++ NinjaHive.Contract/Commands/GetEquipmentItemByIdCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using NinjaHive.Contract.DTOs;
+using NinjaHive.Core.Validations;
+
+namespace NinjaHive.Contract.Commands
+{
+    public class GetEquipmentItemByIdCommand
+    {
+        [Required]
+        [ValidateObject]
+        public Guid EquipmentId;
+    }
+}
