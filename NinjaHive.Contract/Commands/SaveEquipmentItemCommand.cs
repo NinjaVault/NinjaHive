9ae2017d32ffcc75ae8bb7f277025d951617ccb4
Ruggero Visintin <ruggerovisintin@gmail.com>
2015-08-29 12:37:29 +0200

    Add SaveEquipmentItemCommand.cs - now it is possible to save various parameters of an EquipmentItem item

---
+++ NinjaHive.Contract/Commands/SaveEquipmentItemCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using NinjaHive.Contract.DTOs;
+using NinjaHive.Core.Validations;
+
+namespace NinjaHive.Contract.Commands
+{
+    public class SaveEquipmentItemCommand
+    {
+        [Required]
+        [ValidateObject]
+        public EquipmentItem EquipmentItem { get; set; }
+    }
+}
