58952b9216f8fb7df2a24f3c598fe63c44a79d13
Ruggero Visintin <ruggerovisintin@gmail.com>
2015-08-29 13:10:38 +0200

    Add DeleteEquipmentItemCommand.cs - Take the item to remove - TODO pass the item id instead of pass the DTO.

---
+++ NinjaHive.Contract/Commands/DeleteEquipmentItemCommand.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using NinjaHive.Contract.DTOs;
+using NinjaHive.Core.Validations;
+
+namespace NinjaHive.Contract.Commands
+{
+    public class DeleteEquipmentItemCommand
+    {
+        [Required]
+        [ValidateObject]
+        public EquipmentItem EquipmentItem { get; set; }
+    }
+}
