263ea9b3330aef42c6cbb44d9efe4caf56a34c23
Ruggero Visintin <ruggerovisintin@gmail.com>
2015-08-29 12:36:25 +0200

    Add SaveEquipmentItemCommandHandler.cs -  now it is possible to save various parameters of an EquipmentItem item

---
+++ NinjaHive.BusinessLayer/CommandHandlers/SaveEquipmentItemCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NinjaHive.Contract.Commands;
+using NinjaHive.Core;
+using NinjaHive.Domain;
+
+namespace NinjaHive.BusinessLayer.CommandHandlers
+{
+    public class SaveEquipmentItemCommandHandler : ICommandHandler<SaveEquipmentItemCommand>
+    {
+        private readonly NinjaHiveContext db;
+
+        public SaveEquipmentItemCommandHandler(NinjaHiveContext db)
+        {
+            this.db = db;
+        }
+
+        public void Handle(SaveEquipmentItemCommand command)
+        {
+            EquipmentItemEntity item = (EquipmentItemEntity)db.GameItemEntities.Find(command.EquipmentItem.Id);
+
+            item.Name = command.EquipmentItem.Name;
+            item.Description = command.EquipmentItem.Description;
+            item.Category = command.EquipmentItem.Category;
+            item.Craftable = command.EquipmentItem.Craftable;
+            item.IsUpgrader = command.EquipmentItem.UpgradeElement;
+            item.IsCrafter = command.EquipmentItem.CraftingElement;
+            item.IsQuestItem = command.EquipmentItem.QuestItem;
+            item.Durability = command.EquipmentItem.Durability;
+            item.Value = command.EquipmentItem.Value;
+        }
+    }
+}
