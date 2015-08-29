64a074df0617f641993ec644a87b0d459d9ab475
Ruggero Visintin <ruggerovisintin@gmail.com>
2015-08-29 13:09:36 +0200

    Add DeleteEquipmentItemCommandHandler.cs - Removes a given item from the database - TODO pass only the idem id instead all the DTO.

---
+++ NinjaHive.BusinessLayer/CommandHandlers/DeleteEquipmentItemCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NinjaHive.Contract.Commands;
+using NinjaHive.Core;
+using NinjaHive.Domain;
+
+namespace NinjaHive.BusinessLayer.CommandHandlers
+{
+    public class DeleteEquipmentItemCommandHandler 
+        : ICommandHandler<DeleteEquipmentItemCommand>
+    {
+        private readonly NinjaHiveContext db;
+        
+        public DeleteEquipmentItemCommandHandler(NinjaHiveContext db)
+        {
+            this.db = db;
+        }
+
+        public void Handle(DeleteEquipmentItemCommand command)
+        {
+            db.GameItemEntities.Remove(db.GameItemEntities.Find(command.EquipmentItem.Id));
+        }
+    }
+}
