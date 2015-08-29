fb5325f6c56281ae2064ccea06e6c7bc2a883deb
Ruggero Visintin <ruggerovisintin@gmail.com>
2015-08-29 10:48:27 +0200

    Add GetEquipmentItemByIdQuery

---
+++ NinjaHive.Contract/Queries/GetEquipmentItemByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using NinjaHive.Contract.DTOs;
+using NinjaHive.Core;
+
+namespace NinjaHive.Contract.Queries
+{
+    public class GetEquipmentItemByIdQuery : IQuery<EquipmentItem>
+    {
+        public Guid EquipmentItemId;
+    }
+}
