92ef500d74b74087515dc5bfef31ff429bd415b8
Ruggero Visintin <ruggerovisintin@gmail.com>
2015-09-23 00:23:21 +0200

    Update Skill.cs

--- NinjaHive.Contract/DTOs/Skill.cs
+++ NinjaHive.Contract/DTOs/Skill.cs
@@ -5,8 +5,19 @@ namespace NinjaHive.Contract.DTOs
 {
     public class Skill
     {
-        public Guid Id { get; set; }
         [Required]
-        public string Name { get; set; }
+        public Guid Id                  { get; set; }
+        [Required]
+        public string Name              { get; set; }
+        [Required]
+        public int Range                { get; set; }
+        [Required]
+        public int Radius               { get; set; }
+        [Required]
+        public int target               { get; set; }
+        [Required]
+        public int targetCount          { get; set; }
+        [Required]
+        public bool friendly            { get; set; }
     }
 }
