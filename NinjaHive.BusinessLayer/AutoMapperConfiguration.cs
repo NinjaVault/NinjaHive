aecd6bc5137a9251e2484c69840c33e3a749c926
Ruggero <ruggerovisintin@outlook.it>
2015-10-08 20:42:50 +0200

    Update AutoMapperConfiguration.cs     - Added SkillEntity, Skill Map.

--- NinjaHive.BusinessLayer/AutoMapperConfiguration.cs
+++ NinjaHive.BusinessLayer/AutoMapperConfiguration.cs
@@ -15,6 +15,22 @@ namespace NinjaHive.BusinessLayer
                       options => options.MapFrom(source => source.IsCrafter))
                   .ForMember(destination => destination.QuestItem,
                       options => options.MapFrom(source => source.IsQuestItem));
+
+            Mapper.CreateMap<SkillEntity, Skill>()
+                  .ForMember(destination => destination.friendly,
+                      options => options.MapFrom(source => source.Friendly))
+                  .ForMember(destination => destination.Name,
+                      options => options.MapFrom(source => source.Name))
+                  .ForMember(destination => destination.Radius,
+                      options => options.MapFrom(source => source.Radius))
+                  .ForMember(destination => destination.Radius,
+                      options => options.MapFrom(source => source.Radius))
+                  .ForMember(destination => destination.Range,
+                      options => options.MapFrom(source => source.Range))
+                  .ForMember(destination => destination.target,
+                      options => options.MapFrom(source => source.Target))
+                  .ForMember(destination => destination.targetCount,
+                      options => options.MapFrom(source => source.Targets));
         }
-    }
+    };
 }
