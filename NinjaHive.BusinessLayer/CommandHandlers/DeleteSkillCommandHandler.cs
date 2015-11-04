98b2b746ef63a2c9a8ea5f6463a838a43871c34c
Ruggero <ruggerovisintin@outlook.it>
2015-11-04 20:06:33 +0100

    Create DeleteSkillCommandHandler.cs

---
+++ NinjaHive.BusinessLayer/CommandHandlers/DeleteSkillCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using NinjaHive.Contract.Commands;
+using NinjaHive.Core;
+using NinjaHive.Domain;
+
+namespace NinjaHive.BusinessLayer.CommandHandlers
+{
+    public class DeleteSkillCommandHandler
+        : ICommandHandler<DeleteSkillCommand>
+    {
+        private readonly IRepository<SkillEntity> skillEntityRepository;
+
+        public DeleteSkillCommandHandler(IRepository<SkillEntity> skillEntityRepository)
+        {
+            this.skillEntityRepository = skillEntityRepository;
+        }
+
+        public void Handle(DeleteSkillCommand command)
+        {
+            var skillItem = this.skillEntityRepository.GetById(command.Skill.Id);
+            this.skillEntityRepository.Remove(skillItem);
+        }
+    }
+}
