e584af59bc98b0b51ef89d996072afc3369347b0
Ruggero <ruggerovisintin@outlook.it>
2015-11-04 20:07:01 +0100

    Create StatInfo.cs

---
+++ NinjaHive.Contract/DTOs/StatInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using NinjaHive.Core.Validations;
+
+namespace NinjaHive.Contract.DTOs
+{
+    public class StatInfo
+    {
+        [NonEmptyGuid]
+        public Guid Id { get; set; }
+
+        public int Health { get; set; }
+        public int Magic { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int Agility { get; set; }
+        public int Intelligence { get; set; }
+        public int Hunger { get; set; }
+        public int Stamina { get; set; }
+        public int Resistance { get; set; }
+
+    }
+}
