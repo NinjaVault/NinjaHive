9ad14311abe5ff5a93bfe7423cee3ae051967d4e
Ruggero <ruggerovisintin@outlook.it>
2015-11-24 23:49:21 +0100

    Create EditStatCommand.cs

---
+++ NinjaHive.Contract/Commands/EditStatCommand.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using NinjaHive.Contract.DTOs;
+using NinjaHive.Core.Validations;
+
+namespace NinjaHive.Contract.Commands
+{
+    public class EditStatCommand
+    {
+        public EditStatCommand(StatInfo stat, bool createNew)
+        {
+            this.Stat = stat;
+            this.CreateNew = createNew;
+        }
+
+        [Required]
+        [ValidateObject]
+        public StatInfo Stat { get; set; }
+
+        public bool CreateNew { get; set; }
+    }
+}
