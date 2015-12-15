c0166b9cd90e9597080f820b7331cb8a721e2bb0
Ruggero <ruggerovisintin@outlook.it>
2015-12-15 12:11:00 +0100

    Update SkillController.cs - moved all stats functions to skill page

--- NinjaHive.WebApp/Controllers/SkillsController.cs
+++ NinjaHive.WebApp/Controllers/SkillsController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Web.Mvc;
+using System.Collections.Generic;
 using NinjaHive.Contract.Commands;
 using NinjaHive.Contract.DTOs;
 using NinjaHive.Contract.Queries;
 using NinjaHive.Core;
 using NinjaHive.WebApp.Services;
+using NinjaHive.WebApp.Models.Skills;
 
 namespace NinjaHive.WebApp.Controllers
 {
@@ -14,7 +16,7 @@ namespace NinjaHive.WebApp.Controllers
         private readonly ICommandHandler<EditSkillCommand> editSkillCommandHandler;
         private readonly ICommandHandler<DeleteSkillCommand> deleteSkillCommandHandler;
         private readonly ICommandHandler<EditStatCommand> editStatCommandHandler;
-        private readonly ICommandHandler<DeleteStatCommand> deleteStatCommand;
+        private readonly ICommandHandler<DeleteStatCommand> deleteStatCommandHandler;
 
         public SkillsController(
             IQueryProcessor queryProcessor,
@@ -27,50 +29,142 @@ namespace NinjaHive.WebApp.Controllers
             this.editSkillCommandHandler = editSkillCommandHandler;
             this.deleteSkillCommandHandler = deleteSkillCommandHandler;
             this.editStatCommandHandler = editStatCommandHandler;
-            this.deleteStatCommand = deleteStatCommand;
+            this.deleteStatCommandHandler = deleteStatCommandHandler;
         }
 
         // GET: Skills
         public ActionResult Index()
         {
-            var skills = this.queryProcessor.Execute(new GetAllSkillsQuery());
+            var skills = this.queryProcessor.Execute(new GetAllSkillsQuery());      
+            
             return View(skills);
         }
       
         public ActionResult Edit(Guid skillId)
         {
             var skill = this.queryProcessor.Execute(new GetEntityByIdQuery<Skill>(skillId));
+            var stat = this.queryProcessor.Execute(new GetEntityByIdQuery<StatInfo>(skill.StatInfoId));
 
-            return View(skill);
-        }
-
-        public ActionResult Create()
-        {
-            return View();
+            var skillViewModel = new SkillViewModel();
+            UpdateSkillViewModel(skillViewModel, skill);
+            UpdateSkillViewModel(skillViewModel, stat);
+            
+            return View(skillViewModel);
         }
 
         [HttpPost]
-        public ActionResult Create(Skill skill)
+        public ActionResult Edit(SkillViewModel viewModel)
         {
+            var skill = new Skill();
+            UpdateSkill(skill, viewModel);
+
+            var stat = new StatInfo();
+            UpdateStat(stat, viewModel);
+
+            var editSkillcommand = new EditSkillCommand(skill, createNew: false);
+            this.editSkillCommandHandler.Handle(editSkillcommand);
+
+            var editStatCommand = new EditStatCommand(stat, createNew: false);
+            this.editStatCommandHandler.Handle(editStatCommand);
+
+            var redirectUri = UrlProvider<SkillsController>.GetRouteValues(c => c.Index());
+            return RedirectToRoute(redirectUri);
+        }
+
+
+        public ActionResult Create()
+        {
+            var view = new SkillViewModel();
+            return View(view);
+        }
+
+        [HttpPost]
+        public ActionResult Create(SkillViewModel viewModel)
+        {
+            var skill = new Skill();
+            UpdateSkill(skill, viewModel);
+
             skill.Id = Guid.NewGuid();
-            var command = new EditSkillCommand(skill, createNew: true);
-            this.editSkillCommandHandler.Handle(command);
+            var editSkillcommand = new EditSkillCommand(skill, createNew: true);
+            this.editSkillCommandHandler.Handle(editSkillcommand);
+
+            var stat = new StatInfo();
+            UpdateStat(stat, viewModel);
+
+            stat.Id = Guid.NewGuid();
+            var editStatCommand = new EditStatCommand(stat, createNew: true);
+            this.editStatCommandHandler.Handle(editStatCommand);
 
             var redirectUri = UrlProvider<SkillsController>.GetRouteValues(c => c.Index());
             return RedirectToRoute(redirectUri);
         }  
 
-        public ActionResult Delete(Skill skill)
+        public ActionResult Delete(Guid skill)
         {
-            var command = new DeleteSkillCommand()
+            var deleteSkillCommand = new DeleteSkillCommand()
             {
-                Skill = skill
+                Skill = this.queryProcessor.Execute(new GetEntityByIdQuery<Skill>(skill))
             };
 
-            deleteSkillCommandHandler.Handle(command);
+            var deleteStatCommand = new DeleteStatCommand()
+            {
+                Stat = this.queryProcessor.Execute(new GetEntityByIdQuery<StatInfo>(deleteSkillCommand.Skill.StatInfoId))
+            };
+
+            deleteSkillCommandHandler.Handle(deleteSkillCommand);
+            deleteStatCommandHandler.Handle(deleteStatCommand);
 
             var redirectUri = UrlProvider<SkillsController>.GetRouteValues(c => c.Index());
             return RedirectToRoute(redirectUri);
+        }
+
+        private void UpdateSkill(Skill skill, SkillViewModel viewModel)
+        {
+            skill.Id = viewModel.SkillId;
+            skill.Name = viewModel.SkillName;
+            skill.Radius = viewModel.SkillRadius;
+            skill.Range = viewModel.SkillRange;
+            skill.StatInfoId = viewModel.StatId;
+            skill.Target = viewModel.SkillTarget;
+            skill.TargetCount = viewModel.SkillTargetCount;
+        }
+
+        private void UpdateSkillViewModel(SkillViewModel viewModel, Skill skill)
+        {
+            viewModel.SkillId = skill.Id;
+            viewModel.SkillName = skill.Name;
+            viewModel.SkillRadius = skill.Radius;
+            viewModel.SkillRange = skill.Range;
+            viewModel.SkillTarget = skill.Target;
+            viewModel.SkillTargetCount = skill.TargetCount;
+        }
+
+        private void UpdateSkillViewModel(SkillViewModel viewModel, StatInfo stat)
+        {
+            viewModel.StatAgility = stat.Agility;
+            viewModel.StatAttack = stat.Attack;
+            viewModel.StatDefense = stat.Defense;
+            viewModel.StatHealth = stat.Health;
+            viewModel.StatHunger = stat.Hunger;
+            viewModel.StatId = stat.Id;
+            viewModel.StatIntelligence = stat.Intelligence;
+            viewModel.StatMagic = stat.Magic;
+            viewModel.StatResistance = stat.Resistance;
+            viewModel.StatStamina = stat.Stamina;
+        }
+
+        private void UpdateStat(StatInfo stat, SkillViewModel viewModel)
+        {
+            stat.Agility = viewModel.StatAgility;
+            stat.Attack = viewModel.StatAttack;
+            stat.Defense = viewModel.StatDefense;
+            stat.Health = viewModel.StatDefense;
+            stat.Hunger = viewModel.StatHunger;
+            stat.Id = viewModel.StatId;
+            stat.Intelligence = viewModel.StatIntelligence;
+            stat.Magic = viewModel.StatMagic;
+            stat.Resistance = viewModel.StatResistance;
+            stat.Stamina = viewModel.StatStamina;
         }
     }
 }
