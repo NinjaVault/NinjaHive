1e29d5f164067278a1214438ae034bf5a034d8ef
Ruggero <ruggerovisintin@outlook.it>
2015-12-15 12:14:54 +0100

    Add SkillViewModels.cs

---
+++ NinjaHive.WebApp/Models/SkillViewModels.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NinjaHive.WebApp.Models.Skills
+{
+    public class SkillViewModel
+    {
+        public Guid         SkillId { get; set; }
+        public string       SkillName { get; set; }
+        public int          SkillRange { get; set; }
+        public int          SkillRadius { get; set; }
+        public int          SkillTarget { get; set; }
+        public int          SkillTargetCount { get; set; }
+        public bool         SkillFriendly { get; set; }
+
+        public Guid         StatId { get; set; }
+        public int          StatHealth { get; set; }
+        public int          StatMagic { get; set; }
+        public int          StatAttack { get; set; }
+        public int          StatDefense { get; set; }
+        public int          StatAgility { get; set; }
+        public int          StatIntelligence { get; set; }
+        public int          StatHunger { get; set; }
+        public int          StatStamina { get; set; }
+        public double       StatResistance { get; set; }
+    }
+}
