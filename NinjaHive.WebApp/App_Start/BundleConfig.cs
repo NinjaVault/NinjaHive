dcef23362e5d914577a57984d46750ff313009b3
Alper Aslan <Qu4ntumHive@gmail.com>
2015-12-22 12:16:15 +0100

    windows 8 loading css

--- NinjaHive.WebApp/App_Start/BundleConfig.cs
+++ NinjaHive.WebApp/App_Start/BundleConfig.cs
@@ -23,8 +23,9 @@ namespace NinjaHive.WebApp
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                "~/Content/Win8Loader.css",
+                "~/Content/bootstrap.css",
+                "~/Content/site.css"));
         }
     }
 }
