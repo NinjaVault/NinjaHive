8d38abc6e037be8a1d0b83f5c9723e4c1e18efb7
Alper Aslan <Qu4ntumHive@gmail.com>
2016-04-28 16:34:41 +0200

    -refactored equipmentslot into seperate file + readability -removed redundant IModel since the base already implements this

---
+++ NinjaHive.Contract/EquipmentSlot.enum.cs
@@ -0,0 +1,14 @@
+﻿namespace NinjaHive.Contract
+{
+    public enum EquipmentSlot
+    {
+        Head = 0,
+        Neck = 1,
+        Torso = 2,
+        Arms = 3,
+        LeftHand = 4,
+        RightHand = 5,
+        Legs = 6,
+        Feet = 7,
+    }
+}
--- NinjaHive.Contract/Models/EquipmentModel.cs
+++ NinjaHive.Contract/Models/EquipmentModel.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NinjaHive.Contract.Models
+﻿namespace NinjaHive.Contract.Models
 {
-    public enum EquipmentSlot
+    public class EquipmentModel: GameItemModel
     {
-        Head, Neck, Torso, Arms, LeftHand, RightHand, Legs, Feet
-    }
+        public EquipmentModel()
+        {
+            this.Durability = 0;
+            this.Slot = EquipmentSlot.Head;
+        }
 
-    public class EquipmentModel: GameItemModel, IModel
-    {
         public int Durability { get; set; }
         public EquipmentSlot Slot { get; set; }
-
-        public EquipmentModel() { Durability = 0; Slot = 0; }
     }
 }
