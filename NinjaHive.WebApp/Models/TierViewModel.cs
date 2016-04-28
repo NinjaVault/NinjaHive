972fdf063928b71cc23b3f64ed96bb5dc9184c23
Ruggero <ruggerovisintin@outlook.it>
2016-04-28 21:02:30 +0200

    Add TierViewModel.cs

---
+++ NinjaHive.WebApp/Models/TierViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.ComponentModel;
+using NinjaHive.Contract.Models;
+
+namespace NinjaHive.WebApp.Models
+{
+    [ValidateInput(false)]
+    public class TierViewModel
+    {
+        public TierModel TierItem { get; set; }
+    }
+}
