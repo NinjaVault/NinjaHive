0a5a974f923186f79b220e7f5a46217cbc699120
Ruggero <ruggerovisintin@outlook.it>
2016-04-28 21:01:36 +0200

    Add EquipmentMode.cs

---
+++ NinjaHive.Contract/Models/EquipmentModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using NinjaHive.Core.Helpers;
+using NinjaHive.Core.Validations;
+
+namespace NinjaHive.Contract.Models
+{
+    public class EquipmentModel : IModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "{0} is required")]
+        [Range(0, int.MaxValue, ErrorMessage = "Please enter valid integer for {0}. Minimum: {1}")]
+        public int Durability { get; set; }
+
+
+        // Slot: enum? 
+    }
+}
