34fba593b4ea8fb6bf3e7a436233a655532667c3
Alper Aslan <Qu4ntumHive@gmail.com>
2016-04-28 16:35:16 +0200

    models should implement IModel (so LevelModel should too)

--- NinjaHive.Contract/Models/LevelModel.cs
+++ NinjaHive.Contract/Models/LevelModel.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NinjaHive.Contract.Models
 {
-    public class LevelModel
+    public class LevelModel : IModel
     {
         public Guid Id { get; set; }
 
