144b182e8cae9cf94f54126b47b1f241eaf2cecc
Ruggero <ruggerovisintin@outlook.it>
2016-04-28 21:01:44 +0200

    Add LevelModel.cs

---
+++ NinjaHive.Contract/Models/LevelModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using NinjaHive.Core.Helpers;
+using NinjaHive.Core.Validations;
+
+namespace NinjaHive.Contract.Models
+{
+    public class LevelModel : IModel
+    {
+        public Guid Id { get; set; }
+    }
+}
