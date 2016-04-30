753248f1998eef5f85b64dcc0516493360efdcd4
Alper Aslan <Qu4ntumHive@gmail.com>
2016-04-30 11:31:54 +0200

    Reverting of wrong commits of Ruggero

---
+++ NinjaHive.Contract/Models/EquipmentModel.cs
@@ -0,0 +1,16 @@
+﻿using NinjaHive.Components.Enums;
+
+namespace NinjaHive.Contract.Models
+{
+    public class EquipmentModel: GameItemModel
+    {
+        public EquipmentModel()
+        {
+            this.Durability = 0;
+            this.Slot = BodySlot.Head;
+        }
+
+        public int Durability { get; set; }
+        public BodySlot Slot { get; set; }
+    }
+}
