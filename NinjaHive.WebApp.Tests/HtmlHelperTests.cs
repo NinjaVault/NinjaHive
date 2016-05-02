ed12cf1e4a8ec6dabea707d9fc1a11c108e18e10
Alper Aslan <Qu4ntumHive@gmail.com>
2016-05-02 23:22:08 +0200

    Revert of my bad commits

--- NinjaHive.WebApp.Tests/HtmlHelperTests.cs
+++ NinjaHive.WebApp.Tests/HtmlHelperTests.cs
@@ -4,11 +4,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NinjaHive.Contract.Models;
 using NinjaHive.WebApp.Areas.Items.Models;
 using NinjaHive.WebApp.Extensions;
-using NinjaHive.Core.Extensions;
 using NinjaHive.WebApp.Tests.Mocks.Mvc;
-using System;
-using System.Linq.Expressions;
-using NinjaHive.WebApp.Areas.Items.Controllers;
 
 namespace NinjaHive.WebApp.Tests
 {
@@ -16,67 +12,27 @@ namespace NinjaHive.WebApp.Tests
     public class HtmlHelperTests
     {
         [TestMethod]
-        public void GetPropertyNameFromExpression_PropertyFromFirstClass_GetsPropertyName()
-        {
-            // Arrange
-            Expression<Func<EquipmentModel, string>> expression = m => m.Name;
-
-            // Act
-            var name = expression.GetPropertyNameFromExpression();
-
-            // Assert
-            Assert.AreEqual(name, "Name");
-        }
-
-        [TestMethod]
-        public void GetPropertyNameFromExpression_PropertyFromReferencedClass_GetsPropertyName()
-        {
-            // Arrange
-            Expression<Func<EquipmentViewModel, string>> expression = m => m.Item.Name;
-
-            // Act
-            var name = expression.GetPropertyNameFromExpression();
-
-            // Assert
-            Assert.AreEqual(name, "Name");
-        }
-
-        [TestMethod]
-        public void GetPropertyNameFromExpression_ExpressionBodyIsMethodCall_ThrowsArgumentException()
-        {
-            // Arrange
-            Expression<Func<EquipmentController, ActionResult>> expression = c => c.Index();
-
-            var errorMessage = string.Empty;
-
-            // Act
-            try
-            {
-                var name = expression.GetPropertyNameFromExpression();
-            }
-            catch(ArgumentException exception)
-            {
-                errorMessage = exception.Message;
-            }
-
-            // Assert
-            Assert.AreEqual(errorMessage, $"Lambda body '{expression}' is not a MemberExpression");
-        }
-
-        [TestMethod]
-        public void TestMethod1() //TODO create tests for the FormExtensions
+        public void TestMethod1()
         {
             // Arrange
             var viewModel = new EquipmentViewModel(new EquipmentModel(), Enumerable.Empty<CategoryModel>());
 
             var viewData = new ViewDataDictionary(viewModel);
 
-            var html = MvcHelper.GetHtmlHelper(viewModel, new EquipmentController(null, null), true);
+            var html = MvcHelper.GetHtmlHelper(viewModel, new MyTestController(), true);
 
             // Act
-            var output = html.FormGroupForEditor(m => m.Item.Name);
+            var output = html.FormGroupFor(m => m.Item.Name);
 
-            // Assert
+            // Arrange
+        }
+    }
+
+    public class MyTestController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
         }
     }
 }
--- NinjaHive.WebApp/Extensions/FormExtensions.cs
+++ NinjaHive.WebApp/Extensions/FormExtensions.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using NinjaHive.WebApp.Helpers;
+using System;
+using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Text;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
-using NinjaHive.WebApp.Helpers;
 
 namespace NinjaHive.WebApp.Extensions
 {
@@ -22,6 +24,51 @@ namespace NinjaHive.WebApp.Extensions
             var controller = urlParts["controller"].ToString();
             var action = urlParts["action"].ToString();
             return htmlHelper.BeginForm(action, controller, FormMethod.Post, htmlAttributes);
+        }
+
+        public static MvcHtmlString FormGroupFor<TModel, TValue>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expression)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine("<div class='form-group'>");
+            builder.AppendLine(htmlHelper.LabelFor(expression, new { @class = "col-md-4 control-label" }).ToHtmlString());
+            builder.AppendLine("<div class='col-md-8'>");
+            builder.AppendLine(htmlHelper.EditorFor(expression, new { htmlAttributes = new { @class = "form-control" } }).ToHtmlString());
+            builder.AppendLine(htmlHelper.ValidationMessageFor(expression, "", new { @class = "text-danger" }).ToHtmlString());
+            builder.AppendLine("</div></div>");
+
+            return new MvcHtmlString(builder.ToString());
+        }
+
+        public static MvcHtmlString FormGroupFor<TModel>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, bool>> expression)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine("<div class='form-group'>");
+            builder.AppendLine(htmlHelper.EditorFor(expression).ToHtmlString());
+            builder.AppendLine(htmlHelper.LabelFor(expression, new { @class = "control-label" }).ToHtmlString());
+            builder.AppendLine("</div>");
+
+            return new MvcHtmlString(builder.ToString());
+        }
+
+
+        public static MvcHtmlString FormGroupFor<TModel,TValue>(this HtmlHelper<TModel> htmlHelper,
+                                                            Expression<Func<TModel, TValue>> expression,
+                                                            IEnumerable<SelectListItem> list)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine("<div class='form-group'>");
+            builder.AppendLine(htmlHelper.LabelFor(expression, new { @class = "col-md-4 control-label" }).ToHtmlString());
+            builder.AppendLine("<div class='col-md-8'>");
+            
+            builder.AppendLine(htmlHelper.DropDownList(PropertyHelper.NameFromExpression(expression),
+                                                        list,
+                                                        new { @class = "form-control" }
+                                                    ).ToHtmlString()
+                                                );
+            builder.AppendLine(htmlHelper.ValidationMessageFor(expression, "", new { @class = "text-danger" }).ToHtmlString());
+            builder.AppendLine("</div></div>");
+
+            return new MvcHtmlString(builder.ToString());
         }
     }
 }
