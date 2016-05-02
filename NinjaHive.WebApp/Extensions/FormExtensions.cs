ee211ff72fdf7ddabcd3333aa00ca44bf4da5957
Alper Aslan <Qu4ntumHive@gmail.com>
2016-05-02 22:23:13 +0200

    accidentally removed the form extensions, my bad

---
+++ NinjaHive.WebApp/Extensions/FormExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+using NinjaHive.WebApp.Helpers;
+
+namespace NinjaHive.WebApp.Extensions
+{
+    public static class FormExtensions
+    {
+        public static MvcForm BeginForm<TController>(this HtmlHelper htmlHelper, Expression<Action<TController>> expression)
+            where TController : Controller
+        {
+            return htmlHelper.BeginForm(expression, null);
+        }
+
+        public static MvcForm BeginForm<TController>(this HtmlHelper htmlHelper, Expression<Action<TController>> expression, object htmlAttributes)
+            where TController : Controller
+        {
+            var urlParts = UrlProvider<TController>.GetRouteValues(expression);
+
+            var controller = urlParts["controller"].ToString();
+            var action = urlParts["action"].ToString();
+            return htmlHelper.BeginForm(action, controller, FormMethod.Post, htmlAttributes);
+        }
+    }
+}
