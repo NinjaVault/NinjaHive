30238fb326dabe39e3599bafac4a12494b93fa95
Alper Aslan <Qu4ntumHive@gmail.com>
2016-05-04 19:29:53 +0200

    remove old version of modernizr

--- NinjaHive.WebApp/App_Start/BundleConfig.cs
+++ NinjaHive.WebApp/App_Start/BundleConfig.cs
@@ -16,7 +16,7 @@ namespace NinjaHive.WebApp
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/modernizr-{version}"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
